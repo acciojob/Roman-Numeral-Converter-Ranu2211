decedef1910c72e555ba44766452e5fab4e8ca55
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-17 16:07:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,40 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let roman = '';
+	const specialCases = {
+    900: 'CM',
+    400: 'CD',
+    90: 'XC',
+    40: 'XL',
+    9: 'IX',
+    4: 'IV',
+  };
+	 for (const [value, symbol] of Object.entries(specialCases)) {
+    while (num >= value) {
+      roman += symbol;
+      num -= value;
+    }
+  }
+  for (let i = 0; i < Object.keys(obj).length; i++) {
+    const [symbol, value] = obj[i];
+    while (num >= value) {
+      roman += symbol;
+      num -= value;
+    }
+  }
+
+  return roman;
+
+
 
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// You can test your code by running the above 
+//function and printing it to console by pressing 
+//the run button at the top. To run it with input 36,
+//uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
